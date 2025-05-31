aaafc2e67f826e1a77cdf832f7cf31e91de05c36
mehmeterengunduz6 <mehmeterengunduz6@gmail.com>
2025-05-31 17:36:24 +0300

    chapter-9 1/2

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,12 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchLatestInvoices, fetchCardData } from '../../lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
     try {
-        const [revenue, latestInvoices, cardData] = await Promise.all([
-            fetchRevenue(),
+        const [latestInvoices, cardData] = await Promise.all([
             fetchLatestInvoices(),
             fetchCardData()
         ]);
@@ -35,7 +36,7 @@ export default async function Page() {
                     />
                 </div>
                 <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                    <RevenueChart revenue={revenue} />
+                    <Suspense fallback={<RevenueChartSkeleton />}><RevenueChart /></Suspense>
                     <LatestInvoices latestInvoices={latestInvoices} />
                 </div>
             </main>
